f2d2ed321ecf92cd31fd0687ac393e52faec14e2
unknown <corentin.ducatez@epitech.eu>
2024-09-22 20:21:51 +0200

    Add logger support + configured lints inside Cargo toml

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,13 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+pub mod keyboard;
+pub mod audio;
+pub mod log;
+
+use audio::device_manager::DeviceManager;
 use tauri::{Menu, MenuItem};
 
 use crate::keyboard::keyboard_manager::KeyboardManager;
-
-pub mod keyboard;
+use crate::log::init_logger;
 
 fn main() {
+    // Init logger
+    init_logger ();
+    // Init keyboard manager
     let _keyboard_manager = KeyboardManager::new();
+    // Init audio devices manager
+    let _device_manager = DeviceManager::new();
+
     let menu = Menu::new()
       .add_native_item(MenuItem::Quit);
     tauri::Builder::default()
