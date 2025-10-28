d9045212e2bc857509d1c557e7b1c7b2a9724366
Corentin Ducatez <wasta.geek@gmail.com>
2025-10-28 18:28:05 +0100

    rust : Adapt SoundEffect model for data exchange with react

--- app/src-tauri/src/models/sound_effects.rs
+++ app/src-tauri/src/models/sound_effects.rs
@@ -1,11 +1,23 @@
+use chrono::{DateTime, Utc, serde::ts_milliseconds_option};
 use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
 
 #[derive(Clone, Serialize, Deserialize)]
 #[serde(tag = "type")]
 pub enum SoundEffect {
     DoNothing,
-    PlaySound{file: String},
-    IncreaseVolume{volume: u8},
-    DecreaseVolume{volume: u8},
+    PlaySound {
+        name: String,
+        #[serde(rename = "lastModified")]
+        #[serde(with = "ts_milliseconds_option")]
+        last_modified: Option<DateTime<Utc>>,
+        path: Option<PathBuf>,
+    },
+    IncreaseVolume {
+        volume: u8,
+    },
+    DecreaseVolume {
+        volume: u8,
+    },
     ClearAllEffects,
 }
